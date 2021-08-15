82a58477331180c152280e81aaccfeac68c4f16b
rousah <emiliarosavdh@gmail.com>
2021-08-15 15:54:21 +0200

    Basic About me

---
+++ my-portfolio/src/components/AboutMe.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="about-me" class="section is-medium">
+    <h1 class="title">About me</h1>
+    <div class="columns">
+      <div class="column container is-flex is-narrow is-justify-content-center">
+        <figure class="image is-128x128">
+          <img
+            class="is-rounded"
+            src="https://bulma.io/images/placeholders/128x128.png"
+          />
+        </figure>
+      </div>
+      <div class="column">Second column</div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AboutMe",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
