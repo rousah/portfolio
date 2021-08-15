7ceeac545cbcc4681b0a4b7af6ef34d2d32d0903
rousah <emiliarosavdh@gmail.com>
2021-08-15 14:10:45 +0200

    Create navbar

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar :show="showNav"/>
+    <HelloWorld :msg="message" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Navbar,
+  },
+  data() {
+    return {
+      message: "Hello world",
+      showNav: false,
+    };
+  },
+};
 </script>
 
 <style>
@@ -23,6 +31,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ my-portfolio/src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav
+    class="navbar is-transparent"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img
+          src="https://bulma.io/images/bulma-logo.png"
+          width="112"
+          height="28"
+        />
+      </a>
+      <a
+        role="button"
+        class="navbar-burger"
+        v-on:click="showNav = !showNav"
+        v-bind:class="{ 'is-active': showNav }"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" v-bind:class="{ 'is-active': showNav }">
+      <div class="navbar-end">
+        <a class="navbar-item"> About me </a>
+        <a class="navbar-item"> My projects </a>
+        <a class="navbar-item"> My skills </a>
+        <a class="navbar-item"> Contact me </a>
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary"><strong> Download CV </strong></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    show: Boolean,
+  },
+  data() {
+    return {
+      // Computed property based on show value, to not mutate the prop
+      showNav: this.show,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
