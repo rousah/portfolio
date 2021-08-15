38dbc62934f8f14eb48a3e7dd2e7dc47ab4ba45e
rousah <emiliarosavdh@gmail.com>
2021-08-15 16:46:17 +0200

    Create my skills component empty

---
+++ my-portfolio/src/components/MySkills.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>skills</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MySkills',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
