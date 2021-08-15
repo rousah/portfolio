59b2af2f1dd298704b5f538f70e38ad79816dd9d
rousah <emiliarosavdh@gmail.com>
2021-08-15 15:54:08 +0200

    Basic Hero

---
+++ my-portfolio/src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero is-medium">
+    <div class="hero-body">
+      <p class="title">Welcome!</p>
+      <p class="subtitle">Medium subtitle</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
