7f75b753e70b3c0bde6be680ee482239b1674aed
rousah <emiliarosavdh@gmail.com>
2021-08-17 13:36:49 +0200

    Create skills section

--- my-portfolio/src/components/MySkills.vue
+++ my-portfolio/src/components/MySkills.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <h1>skills</h1>
-  </div>
+  <section id="skills" class="section is-medium">
+    <h1 class="title">My skills</h1>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'MySkills',
-  props: {
-  }
-}
+  name: "MySkills",
+  props: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
