88592a8fde45553ccbdd5168196d4e1fe52116a7
rousah <emiliarosavdh@gmail.com>
2021-08-24 15:28:29 +0200

    Adding emojis to about me

--- my-portfolio/src/components/AboutMe.vue
+++ my-portfolio/src/components/AboutMe.vue
@@ -8,13 +8,16 @@
         </figure>
       </div>
       <div class="column">
-        Hello! My name is Rosa, I am a Software Developer. I have recently
+        Hello! My name is Rosa, I am a Software Developer 👩🏼‍💻. I have recently
         graduated from the Polythecnic University of Valencia (Spain) in
         Interactive Technologies Engineering, and am currently doing a Front-end
-        internship at The Selection Lab in Amsterdam. I am mainly attracted to
-        web, app and game development, with a special focus on creating
-        attractive and fun UI's. I love working in teams in an agile
-        environment, drinking tea and practicing yoga.
+        internship at
+        <a href="https://www.theselectionlab.com" target="_blank">
+          The Selection Lab
+        </a>
+        in Amsterdam 📍. I am mainly attracted to web, app and game development,
+        with a special focus on creating attractive and fun UI's. I love working
+        in teams in an agile environment, drinking tea and practicing yoga 🧘🏼‍♀️.
       </div>
     </div>
   </section>
