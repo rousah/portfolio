68dfb17a98650b0deca24b6accb08992e72b9f14
rousah <emiliarosavdh@gmail.com>
2024-08-11 18:06:51 +0200

    remove everything

--- my-portfolio/cypress/integration/homepage_spec.js
+++
@@ -1,8 +0,0 @@
-// Comment to tell eslint im using ui bdd (describe, it, etc.)
-/* eslint-env mocha */
-/* global cy */
-describe("The home page", function() {
-  it("Successfully loads", function() {
-    cy.visit("/");
-  });
-});
