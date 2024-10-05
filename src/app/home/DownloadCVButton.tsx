581176c6dcb2d3b701a0b0d1bb2ea2980143bbb4
rousah <emiliarosavdh@gmail.com>
2024-10-05 09:02:20 +0200

    create downlod cv button style

---
+++ src/app/home/DownloadCVButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '../../../tailwind.config';
+import { Colors } from '@/app/types/colors';
+const twFullConfig = resolveConfig(tailwindConfig);
+
+export default function DonwloadCVButton() {
+  const downloadCVText = 'Download CV!!!';
+  const whiteShadow = `0px -5px 0px ${twFullConfig.theme.colors[Colors.white]}`;
+  const pinkShadow = `0px -10px 0px ${twFullConfig.theme.colors[Colors.pink]}`;
+
+  const doubleShadow = `${whiteShadow}, ${pinkShadow}`;
+  const textShadow = {
+    textShadow: doubleShadow,
+  };
+
+  return (
+    <button className="bg-yellow text-3xl text-black pt-5 pb-3 px-7">
+      <span style={textShadow}>{downloadCVText}</span>
+    </button>
+  );
+}
