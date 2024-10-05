07c209e09e8b7749632ba24a9a13715521eec345
rousah <emiliarosavdh@gmail.com>
2024-10-05 12:45:16 +0200

    add about me content

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -1,3 +1,18 @@
+import Heading1 from '@/app/components/text/Heading1';
+import content from '@/app/about_me/content.json';
+
 export default function AboutMe() {
-  return 'about me';
+  return (
+    <main className="bg-blue min-w-full min-h-full text-black">
+      <div className="h-screen p-8 md:p-14 pt-0 md:pt-0">
+        <Heading1>About me</Heading1>
+        <div className="whitespace-pre-wrap font-lora">
+          {content.description}
+        </div>
+        <div>
+          <img></img>
+        </div>
+      </div>
+    </main>
+  );
 }
