039b2e2c6afc253d639e6ca42014c6e1585c5d74
rousah <emiliarosavdh@gmail.com>
2024-10-05 12:53:27 +0200

    add email and image to about me page

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -1,16 +1,25 @@
 import Heading1 from '@/app/components/text/Heading1';
 import content from '@/app/about_me/content.json';
+import aboutMeImage from '@/app/assets/about_me/about_me.png';
+import Image from 'next/image';
 
 export default function AboutMe() {
   return (
     <main className="bg-blue min-w-full min-h-full text-black">
-      <div className="h-screen p-8 md:p-14 pt-0 md:pt-0">
+      <div className="min-h-screen p-8 md:p-14 pt-0 md:pt-0 pb-0">
         <Heading1>About me</Heading1>
         <div className="whitespace-pre-wrap font-lora">
           {content.description}
+          <br />
+          <br />
+          emiliarosavdh@gmail.com
         </div>
         <div>
-          <img></img>
+          <Image
+            src={aboutMeImage}
+            alt="placeholder"
+            style={{ width: '100%', height: 'auto' }}
+          />
         </div>
       </div>
     </main>
