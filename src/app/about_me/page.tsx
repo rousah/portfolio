f26dcd986667bf6c91725b3cbcdcdacb413a7632
rousah <emiliarosavdh@gmail.com>
2024-10-06 20:41:44 +0200

    use page layout for about me page

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -2,14 +2,14 @@ import Heading1 from '@/app/components/text/Heading1';
 import content from '@/app/about_me/content.json';
 import aboutMeImage from '@/app/assets/about_me/about_me.png';
 import Image from 'next/image';
+import PageLayout from '@/app/components/page/PageLayout';
 
 export default function AboutMe() {
   // TODO: improve description
   // TODO: make email clickable
   return (
-    <main className="bg-blue min-w-full min-h-full text-black">
-      <div className="min-h-screen p-8 md:p-14 pt-0 md:pt-0 pb-0">
-        <Heading1>About me</Heading1>
+    <PageLayout color="blue">
+      <div className="min-h-screen text-black">
         <div className="whitespace-pre-wrap font-lora">
           {content.description}
           <br />
@@ -24,6 +24,6 @@ export default function AboutMe() {
           />
         </div>
       </div>
-    </main>
+    </PageLayout>
   );
 }
