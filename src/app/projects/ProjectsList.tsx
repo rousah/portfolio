3bb03bbbe9c054dc23c33b13840fef669637849b
rousah <emiliarosavdh@gmail.com>
2024-10-09 22:29:05 +0200

    smooth open close project wip

--- src/app/projects/ProjectsList.tsx
+++ src/app/projects/ProjectsList.tsx
@@ -25,34 +25,47 @@ export default function ProjectsList() {
           borderClassName={borderClassNames}
         ></ProjectCard>
       </button>
-      {expandedProjectId === project.id && (
-        <>
-          <ProjectExpansion
-            project={project}
-            borderClassName={borderClassNames}
-          ></ProjectExpansion>
-          <button
-            className={`${borderClassNames} border-t-0 border-green w-full text-green flex justify-center p-1`}
-            onClick={() => handleCardClick(project.id)}
+      <div
+        className={`project-details ${
+          expandedProjectId === project.id ? 'open' : ''
+        }`}
+      >
+        <ProjectExpansion
+          project={project}
+          borderClassName={borderClassNames}
+        ></ProjectExpansion>
+        <button
+          className={`${borderClassNames} border-t-0 border-green w-full text-green flex justify-center p-1`}
+          onClick={() => handleCardClick(project.id)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-circle-chevron-up"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="3"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-circle-chevron-up"
-            >
-              <circle cx="12" cy="12" r="10" />
-              <path d="m8 14 4-4 4 4" />
-            </svg>
-          </button>
-        </>
-      )}
+            <circle cx="12" cy="12" r="10" />
+            <path d="m8 14 4-4 4 4" />
+          </svg>
+        </button>
+      </div>
+      <style jsx>{`
+        .project-details {
+          transition: max-height 0.4s ease-in-out;
+          overflow: hidden;
+          max-height: 0;
+        }
+
+        .project-details.open {
+          max-height: 1000px;
+        }
+      `}</style>
     </div>
   ));
 }
