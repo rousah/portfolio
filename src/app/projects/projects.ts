dffc8888628af3203ee59377a9138d5aa5eb0f4d
rousah <emiliarosavdh@gmail.com>
2024-10-09 20:09:19 +0200

    real project content

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -4,7 +4,7 @@ import { StaticImageData } from 'next/image';
 const projectsWithImageStrings = content;
 const projects = projectsWithImageStrings.map((project) => {
   const image =
-    require(`@/app/assets/${project.image_id}/${project.image_id}.png`).default;
+    require(`@/app/assets/projects/${project.image_id}.png`).default;
 
   return {
     ...project,
