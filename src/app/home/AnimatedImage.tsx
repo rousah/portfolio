0123df5648e038c301b536467afe9870bba33005
rousah <emiliarosavdh@gmail.com>
2024-10-13 22:10:55 +0200

    animate images

--- src/app/home/AnimatedImage.tsx
+++ src/app/home/AnimatedImage.tsx
@@ -1,6 +1,5 @@
 import Image, { StaticImageData } from 'next/image';
 import React, { useEffect, useState } from 'react';
-import '@/app/home/AnimatedImage.css';
 
 interface AnimatedPhotoProps {
   src: StaticImageData;
@@ -12,18 +11,33 @@ const AnimatedImage = ({ src }: AnimatedPhotoProps) => {
     saturate: 1,
   });
 
+  const getRandomInteger = (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
   useEffect(() => {
     const applyTurbulence = () => {
       const randomBrightness = Math.random() * 0.2 + 0.9;
       const randomSaturate = Math.random() * 1.5 + 0.5;
+
       setFilter({
         brightness: randomBrightness,
         saturate: randomSaturate,
       });
     };
 
-    const interval = setInterval(applyTurbulence, 1000);
-    return () => clearInterval(interval);
+    const filterInterval = () => {
+      const time = getRandomInteger(1000, 3000);
+      applyTurbulence();
+      setTimeout(() => {
+        filterInterval();
+      }, time);
+    };
+
+    filterInterval();
   }, []);
 
   return (
@@ -34,6 +48,7 @@ const AnimatedImage = ({ src }: AnimatedPhotoProps) => {
         width: '100%',
         height: 'auto',
         filter: `brightness(${filter.brightness}) saturate(${filter.saturate})`,
+        transition: '-webkit-filter 1000ms ease-in-out',
       }}
       priority={true}
       className="aspect-4/3 object-contain animated-photo"
