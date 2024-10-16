0a7bca511922d097e2aa28ae9ebf2060496a5a48
rousah <emiliarosavdh@gmail.com>
2024-10-16 21:06:40 +0200

    fix mobile photos porject

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
     <PageLayout color="purple">
       <div className="min-w-full border-purple md:border-b-[3.5rem]">
-        <div className="bg-white p-4 md:p-10 grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="bg-white p-4 md:p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <ProjectsList></ProjectsList>
         </div>
       </div>
